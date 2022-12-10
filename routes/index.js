73b31751fe76883eeebe198b804935b63df89eb5
Eyitayo Itunu Babatope <112722909+eyitayoit-alt@users.noreply.github.com>
2022-12-10 14:44:28 +0100

    Update index.js
    Adding routes to app

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
+
 var express = require('express');
 var router = express.Router();
-
+var path = require('path');
+const landingController = require('../controllers/landing');
+const loginController = require('../controllers/login');
+const userController = require('../controllers/userscontroller');
+//const donateController = require('../controllers/donate');
+router.use(express.static('public'));
+router.use(express.static('frontend'));
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Alumni Donation Page' });
-});
+router.get('/', landingController.landingCont);
+router.get('/login',loginController.loginCont );
+//router.get('/donate-new',donateController.donateCont );
+module.exports = router;
+
 
 module.exports = router;

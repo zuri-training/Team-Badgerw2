d4f76a2975a0bb5c0fc32ee0ba660508fb2da0e9
Eyitayo Itunu Babatope <112722909+eyitayoit-alt@users.noreply.github.com>
2022-12-11 18:08:16 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+require('./config/passport');
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const connectToDb = require('./dbConnect')
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');

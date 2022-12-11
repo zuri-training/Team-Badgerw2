f39b19cca149ca941cc12a174079b5972f9596a0
Eyitayo Itunu Babatope <112722909+eyitayoit-alt@users.noreply.github.com>
2022-12-11 17:54:47 +0100

    Update model.js

--- models/model.js
+++ models/model.js
@@ -45,7 +45,35 @@ const cardsSchema = new Schema(
   },
   { timestamps: true }
 );
+alumniSchema.methods.setPassword = function(password) {
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+};
 
+alumniSchema.methods.validatePassword = function(password) {
+  const hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+  return this.hash === hash;
+};
+
+alumniSchema.methods.generateJWT = function() {
+  const today = new Date();
+  const expirationDate = new Date(today);
+  expirationDate.setDate(today.getDate() + 60);
+
+  return jwt.sign({
+    email: this.email,
+    id: this._id,
+    exp: parseInt(expirationDate.getTime() / 1000, 10),
+  }, 'secret');
+}
+
+alumniSchema.methods.toAuthJSON = function() {
+  return {
+    _id: this._id,
+    email: this.email,
+    token: this.generateJWT(),
+  };
+};
 const alumni = mongoose.model("alumni", alumniSchema);
 const donations = mongoose.model("donations", donationsSchema);
 const cards = mongoose.model("cards", cardsSchema);

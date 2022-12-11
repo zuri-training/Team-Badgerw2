56a83a8badc5327563fd0f8ca59265d0eb3c8023
Eyitayo Itunu Babatope <112722909+eyitayoit-alt@users.noreply.github.com>
2022-12-11 18:35:42 +0100

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var userscontroller=require('../controllers/usercontroller');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/',"usercontroller/") 
 
 module.exports = router;

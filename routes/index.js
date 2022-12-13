affa2301c2f67d680abd35e460c6b22b61f30923
Eyitayo Itunu Babatope <112722909+eyitayoit-alt@users.noreply.github.com>
2022-12-13 13:08:27 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,15 @@
-
 var express = require('express');
 var router = express.Router();
 var path = require('path');
-var auth=require('auth')
+
+
+const donationFormController = require('../controllers/donationpageform');
 const landingController = require('../controllers/landing');
-const loginController = require('../controllers/login');
-const userController = require('../controllers/userscontroller');
-//const donateController = require('../controllers/donate');
-router.use(express.static('public'));
-
+const pageController = require('../controllers/createpage');
+;
+router.use(express.static(path.join(__dirname, '../public')));
 /* GET home page. */
-router.get('/',auth.optional, landingController.landingCont);
-router.get('/login',auth.optional,userComtroller.login);
-router.get('/signup',auth.optional,userComtroller.signup);
-//router.get('/donate-new',auth.required, donateController.donateCont );
-
-
-
+router.get('/', landingController.landingcont);
+router.get('/donationform', donationFormController.donationPageCont);
+router.get('/create-donation', pageController.createPageCont);
 module.exports = router;

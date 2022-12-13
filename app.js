05796fba1a3c8518e27ccf9312e5ef874f468242
Eyitayo Itunu Babatope <112722909+eyitayoit-alt@users.noreply.github.com>
2022-12-13 10:52:52 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-require('./config/passport');
-require('./model/model');
+
+require('./models/model');
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -9,7 +9,7 @@ const connectToDb = require('./dbConnect')
 
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var authRouter = require('./routes/auth');
 
 var app = express();
 const PORT = process.env.PORT || 7000
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', authRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

040cde060d254eb054ad889a7c63c124540fe396
Eyitayo Itunu Babatope <112722909+eyitayoit-alt@users.noreply.github.com>
2022-12-13 14:08:28 +0100

    Delete users.js

--- routes/users.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var userscontroller=require('../controllers/usercontroller');
-var auth=require('auth')
-/* GET users listing. */
-router.post('/users/signup',auth.optional, "usercontroller.signup") ;
-router.post('/users/login',auth.optional, "usercontroller.login") ;
-
-module.exports = router;

08dfe70bb00c399ad31b74debb5d862bd00f93a2
Eyitayo Itunu Babatope <112722909+eyitayoit-alt@users.noreply.github.com>
2022-12-13 10:56:44 +0100

    Update model.js

--- models/model.js
+++ models/model.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
-const jwt = require('jsonwebtoken');
+
 
 const alumniSchema = new Schema(
   {
@@ -46,25 +46,7 @@ const cardsSchema = new Schema(
 );
 
 
-alumniSchema.methods.generateJWT = function() {
-  const today = new Date();
-  const expirationDate = new Date(today);
-  expirationDate.setDate(today.getDate() + 60);
 
-  return jwt.sign({
-    email: this.email,
-    id: this._id,
-    exp: parseInt(expirationDate.getTime() / 1000, 10),
-  }, 'secret');
-}
-
-alumniSchema.methods.toAuthJSON = function() {
-  return {
-    _id: this._id,
-    email: this.email,
-    token: this.generateJWT(),
-  };
-};
 const alumni = mongoose.model("alumni", alumniSchema);
 const donations = mongoose.model("donations", donationsSchema);
 const cards = mongoose.model("cards", cardsSchema);

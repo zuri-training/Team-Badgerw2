489403f0c7315e3de9c64745fe2815f79b49039d
Eyitayo Itunu Babatope <112722909+eyitayoit-alt@users.noreply.github.com>
2022-12-17 01:53:24 +0800

    Update usercontroller.js

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -38,7 +38,7 @@ const signup = async (req, res) => {
     }*/
     const oldUser = await alumni.findOne({ email });
     if (oldUser) {
-      req.flash("failure",Alumni Exist, Please Login. ");
+      req.flash("failure",'Alumni Exist, Please Login. ");
     }
     const hashpassword = await bcrypt.hash(password, 12);
     const newUser = new alumni({
@@ -60,7 +60,7 @@ const signup = async (req, res) => {
     //req.flash('success,`You have succesfully registered ${user.firstname}`);
     res.redirect('dashboard',{user:user})
   } catch (error) {
-    req.flash('error',An error occur, cannot register user');
+    req.flash('error','An error occur, cannot register user');
   }
 };
 
@@ -71,20 +71,20 @@ const login = async (req, res) => {
       const user = await alumni.findOne({ email: req.body.email });
   
       if (!user) {
-         res.flash("Wrong Details, Try Again");
+         res.flash('error',"Wrong Details, Try Again");
       }
   
       const match = await bcrypt.compare(req.body.password, user.password);
   
       if (!match) {
-        req.flash("Wrong password, Try Again");
+        req.flash('error',"Wrong password, Try Again");
       }
   
       const { password, ...others } = user._doc;
       res.flash('Login succesful');
       res.redirect('dashboard',{user:user});
     } catch (error) {
-      req.flash(error.message);
+      req.flash('error',error.message);
     }
   };
 

aa3228fd433b44391c8012d3c30fd9dc8b79ef72
Eyitayo Itunu Babatope <112722909+eyitayoit-alt@users.noreply.github.com>
2022-12-21 19:23:48 +0100

    Update donationpageform.js

--- controllers/donationpageform.js
+++ controllers/donationpageform.js
@@ -1,13 +1,26 @@
-var path = require('path');
+var donation = require('../models/model').donations;
 exports.donationPageCont = (req, res) => {
 
   req.on('error', (error) => {
     console.log(error)
-    res.sendStatus(400)
+    res.sendStatus(500)
   }
-  )
+  );
+
+try {
+    const donors = await donation.find();
+    const amount = donors.amount
+    res.render('donation', {
+      title: "Donation Form",
+      css: '/stylesheets/donation.css',
+      donors: donors.length,
+      amounts: 0
+    });
+  } catch (error) {
+    res.render('error', { message: "An error Occured", error: error });
+  }
 
 
-  res.render('donation');
+
 
 }

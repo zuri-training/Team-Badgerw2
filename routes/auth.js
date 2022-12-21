1880d79a54a5d46c0fe8f67e266e0172019297a3
Eyitayo Itunu Babatope <112722909+eyitayoit-alt@users.noreply.github.com>
2022-12-21 11:05:27 +0100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -43,45 +43,112 @@ passport.deserializeUser(function (id, done) {
   });
 });
 function isLoggedIn(req, res, next) {
-  if (req.isAuthenticated()) return next();
-  res.redirect("/login");
+  if (req.session.user) return next();
+  res.render("login", {
+    message: " ",
+    error: "Unauthorised Access"
+  });
 }
+router.get('/flash', function (req, res) {
+  req.flash({
+    "login-success": "Login Succesful",
+    "login-failure": "Login UnSuccesful",
+    'register-success': "Registraion successful",
+    'register-falure': "Registraion successful",
+  });
+  res.redirect('/login');
+  res.redirect('/signup');
+})
 router.get('/login', loginController.loginform);
 
-router.post('/login/password', userController.login);
-router.get('/donationform', donationFormController.donationPageCont);
-router.get('/createpage', pageController.createPageCont);
+router.post('/login', userController.login);
+router.get('/donation', isLoggedIn, donationFormController.donationPageCont);
+router.get('/createpage', isLoggedIn, pageController.createPageCont);
 
 
-router.get('/about',function(req,res){
-  res.render('about');
+router.get('/about', function (req, res) {
+  res.render('about', {
+    title: "About",
+    css: '/stylesheets/about.css'
+  });
+
 })
-router.get('/faculties',function(req,res){
-  res.render('faculties');
+router.get('/faculties', function (req, res) {
+  res.render('faculties', {
+    title: "Faculties",
+    css: '/stylesheets/about.css'
+  });
+
 })
 
 
 router.get('/signup', signupController.signUpCont);
 router.post('/signup/alumni', userController.signup);
-router.get('/about', function (req, res) {
-  res.render('about');
-})
-router.get('/faculties', function (req, res) {
-  res.render('faculties');
-})
+
+router.get('/dashboard', function (req, res) {
+  res.redirect('/login')
+});
+
 router.get('/academic', function (req, res) {
-  res.render('academic');
+  res.render('academic', {
+    title: "Academic",
+    css: '/stylesheets/about.css'
+  });
+
 })
-router.get('/payment', function (req, res) {
-  res.render('payment');
-})
-router.get('/donate', function (req, res) {
-  res.render('donate');
+router.get('/donate', isLoggedIn, function (req, res) {
+  console.log(req.session.user)
+  res.render('donate', {
+    title: "Donate",
+    css: '/stylesheets/donate.css'
+  });
+
 })
 router.get('/alumni', function (req, res) {
-  res.render('alumni');
+  res.render('alumni', {
+    title: "Alumni",
+    css: '/stylesheets/about.css'
+  });
+
 })
 router.get('/contact', function (req, res) {
-  res.render('contact');
+  res.render('contact', {
+    title: "About",
+    css: '/stylesheets/about.css'
+  });
+
+})
+
+
+
+router.post('/payment', function (req, res) {
+  res.render('payment', {
+    title: "Payment",
+    css: '/stylesheets/payment.css',
+    name: req.body.fullname
+  });
+})
+router.get('/share', function (req, res) {
+
+  res.render('sharelink')
+});
+
+router.get('/logout', function (req, res, next) {
+  // logout logic
+
+  // clear the user from the session object and save.
+  // this will ensure that re-using the old session id
+  // does not have a logged in user
+  req.session.user = null
+  req.session.save(function (err) {
+    if (err) next(err)
+
+    // regenerate the session, which is good practice to help
+    // guard against forms of session fixation
+    req.session.regenerate(function (err) {
+      if (err) next(err)
+      res.redirect('/login')
+    })
+  })
 })
 module.exports = router;
